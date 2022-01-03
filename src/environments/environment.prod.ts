909b109d81f5937a0597f122d2aa2e71dad6af8e
Manoj Majumdar <manojmajumdarb92@gmail.com>
2022-01-03 15:32:52 +0530

    changed prod urls

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  websocketapi: 'https://filmbuff-auth.herokuapp.com/api/websocket',
-  auth: 'https://filmbuff-auth.herokuapp.com/auth/',
-  api: 'https://filmbuff-auth.herokuapp.com/api/v1/'
+  websocketapi: 'http://filmbuff-auth.herokuapp.com/api/websocket',
+  auth: 'http://filmbuff-auth.herokuapp.com/auth/',
+  api: 'http://filmbuff-auth.herokuapp.com/api/v1/'
 
 };
